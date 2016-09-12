75a9c7555d0fd435b22b5b9318db271ac1dc65ee
Augustin <augustin@MacBook-Augustin.local>
2016-09-13 00:15:24 +0200

    week 1 homework

---
+++ Session1_HW/Hello.R
@@ -0,0 +1,9 @@
+""
+Origin: first program written for the course Programming for Economists - Fall 2016.
+Filename: Hello.R
+Author: Augustin Dao
+Creation date: 09 September, 2016 
+Last modified: 12 September, 2016
+""
+
+print("Hello world")
