81570641c495f6e4500bf52d280a089b0b059dda
augustindao <augustin@MacBook-Augustin.local>
2016-12-19 01:15:46 +0400

    HW11-bis
    Replace blank file with HW11 Python notebook

--- Session11_HW/Hello.R
+++
@@ -1,9 +0,0 @@
-""
-Origin: Homework 11 - Programming for Economists - Fall 2016.
-Filename: Hello.R
-Author: Augustin Dao
-Creation date: 02 Dec. 2016 
-Last modified: 12 Dec. 2016
-""
-#Sorry didn't really have time to work on this...
-print("Blank file")
